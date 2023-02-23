6823e09c29a0e4f7601b6a2396b8319a88651f11
pedro <pedro4032@hotmail.com>
2023-02-23 16:11:54 -0300

    first commit

---
+++ functional-test/src/test/java/br/com/tasks/functional/TasksTest.java
@@ -0,0 +1,118 @@
+package br.com.tasks.functional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TasksTest {
+
+    public WebDriver acessarAplicacao() {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://localhost:8001/tasks/");
+        return driver;
+    }
+
+    @Test
+    public void deveSalvarTarefaComSucesso() throws InterruptedException {
+        WebDriver driver = acessarAplicacao();
+        Thread.sleep(1500);
+
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            Thread.sleep(1500);
+
+            driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2023");
+
+            driver.findElement(By.id("saveButton")).click();
+            Thread.sleep(1500);
+
+            String message = driver.findElement(By.id("message")).getText();
+
+            Assert.assertEquals("Success!", message);
+            Thread.sleep(1500);
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemDescricao() throws InterruptedException {
+        WebDriver driver = acessarAplicacao();
+        Thread.sleep(1500);
+
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            Thread.sleep(1500);
+
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2023");
+
+            driver.findElement(By.id("saveButton")).click();
+            Thread.sleep(1500);
+
+            String message = driver.findElement(By.id("message")).getText();
+
+            Assert.assertEquals("Fill the task description", message);
+            Thread.sleep(1500);
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaSemData() throws InterruptedException {
+        WebDriver driver = acessarAplicacao();
+        Thread.sleep(1500);
+
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            Thread.sleep(1500);
+
+            driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+
+            driver.findElement(By.id("saveButton")).click();
+            Thread.sleep(1500);
+
+            String message = driver.findElement(By.id("message")).getText();
+
+            Assert.assertEquals("Fill the due date", message);
+            Thread.sleep(1500);
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+    @Test
+    public void naoDeveSalvarTarefaComDataPassada() throws InterruptedException {
+        WebDriver driver = acessarAplicacao();
+        Thread.sleep(1500);
+
+        try {
+            driver.findElement(By.id("addTodo")).click();
+            Thread.sleep(1500);
+
+            driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+
+            driver.findElement(By.id("dueDate")).sendKeys("10/10/2010");
+
+            driver.findElement(By.id("saveButton")).click();
+            Thread.sleep(1500);
+
+            String message = driver.findElement(By.id("message")).getText();
+
+            Assert.assertEquals("Due date must not be in past", message);
+            Thread.sleep(1500);
+        } finally {
+            driver.quit();
+        }
+
+    }
+}
